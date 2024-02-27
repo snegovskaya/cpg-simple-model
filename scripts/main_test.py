42dd6c4fbcbd97333fc1d55ce3fc1014a79593d6
Daria Snegovskaya <snegovskaya.da@gmail.com>
2024-02-27 10:45:20 +0300

    Intermediate stage of connecting Muscle class to odeint

--- scripts/main_test.py
+++ scripts/main_test.py
@@ -1,43 +1,56 @@
 from src import muscle 
 from src import neuron
 
-neuron.neuron_test()
-muscle.muscle_test()
-print("I'm main")
+# neuron.neuron_test()
+# muscle.muscle_test()
+# print("I'm main")
+
 
 from numpy import *
 from matplotlib import pyplot as p
 from scipy import integrate
 
-t = linspace(0, 100, 1000)
-C_N0 = 1 # Чему, ёлы-палы, может быть равен C_N?
+# t = linspace(0, 100, 1000)
+# C_N0 = 1 # Чему, ёлы-палы, может быть равен C_N?
+# F0 = 0
+# X0 = array([C_N0, F0])
+# vrest = -63.0540942 # По книге vrest = -65.0
+# mrest = 0.06 # По книге mrest = 0.05
+# nrest = 0.00 # По книге nrest = 0.00
+# hrest = 0.54 # По книге hrest = 0.62
+# Y0 = array([vrest,mrest,nrest,hrest])
+# t = linspace(0, 100, 1000) # Временная шкала
+# Iapp = 4
+
+# X =  integrate.odeint(muscle.dX_dt, X0, t) 
+# C_N, F = X.T
+# Y =  integrate.odeint(neuron.dX_dt, Y0, t, args=(Iapp,)) # args=(Iapp, tauInput)
+# v, m, n, h = Y.T 
+# def u(t):
+#     return  abs(sin(t*2))
+
+# fig1 = p.figure()
+# p.plot(t, v)
+# p.show(block=True)
+
+# fig2 = p.figure()
+# p.plot(t, u(t), label = '$u, \: \mathrm{мВ}$')
+# p.plot(t, F*500, label = '$F (x500), \: \mathrm{?}$')
+# p.legend()
+# p.xlabel('$t, \: \mathrm{мс}$')
+# p.ylabel('$u, \: F$')
+# p.ylim([-1, 1])
+# p.title('Мышечная сила и входное напряжение, зависимость от времени')
+# p.show(block=True)
+
+t = linspace(0, 100, 100)
+CN0 = 1
 F0 = 0
-X0 = array([C_N0, F0])
-vrest = -63.0540942 # По книге vrest = -65.0
-mrest = 0.06 # По книге mrest = 0.05
-nrest = 0.00 # По книге nrest = 0.00
-hrest = 0.54 # По книге hrest = 0.62
-Y0 = array([vrest,mrest,nrest,hrest])
-t = linspace(0, 100, 1000) # Временная шкала
-Iapp = 4
-
-X =  integrate.odeint(muscle.dX_dt, X0, t) 
-C_N, F = X.T
-Y =  integrate.odeint(neuron.dX_dt, Y0, t, args=(Iapp,)) # args=(Iapp, tauInput)
-v, m, n, h = Y.T 
 def u(t):
-    return  abs(sin(t*2))
+    return sin(t)
 
-fig1 = p.figure()
-p.plot(t, v)
-p.show(block=True)
+print(size(u(t)))
 
-fig2 = p.figure()
-p.plot(t, u(t), label = '$u, \: \mathrm{мВ}$')
-p.plot(t, F*500, label = '$F (x500), \: \mathrm{?}$')
-p.legend()
-p.xlabel('$t, \: \mathrm{мс}$')
-p.ylabel('$u, \: F$')
-p.ylim([-1, 1])
-p.title('Мышечная сила и входное напряжение, зависимость от времени')
-p.show(block=True)
+init_conds = array([CN0, F0])
+test_item = muscle.Muscle(CN0, F0, u)
+test_result = integrate.odeint(test_item.model, init_conds, t)
--- src/muscle.py
+++ src/muscle.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import *
 
 def muscle_test():
     print("I'm a Simplified Adapted Model muscle")
@@ -14,29 +14,31 @@ class Muscle:
     A = 10 # FIXME units! 
     m = 2 # dim-less 
 
-    def __init__(self, t, CN0, F0):
+    def __init__(self, CN0, F0, u): # FIXME: В каком формате передавать u? Как название ф-ции?
         """ 
         Parameters:
-        t : array of time points at those muscle response is observed 
-        CN0 : array of CN(t) meanings in zero approximation 
-        F0 : array of F(t) meanings in zero approximation 
+        CN0 : initial CN(t) meaning 
+        F0 : initial F(t) meaning 
+        u : input voltage function 
         """
+
         self.test = 'I\'m class Muscle test' 
 
-        self.t = t
         self.CN = CN0 
         self.F = F0 
+        self.u = u # 1. FIXME Но это не точно; 2. Зависимость от t сюда вроде как писать не нужно 
 
-    def eq_CN(self, u): 
+    def eq_CN(self, t): 
         """
-        ODE for CN variable
+        Right part of an ODE for CN variable
         FIXME Я пока ХЗ, откуда нужно брать и как сюда внедрять u 
         """
         CN = self.CN 
         tauc = self.tauc 
-        t = self.t 
+        u = self.u 
 
         return - CN / tauc + u(t) 
+        # FIXME: вообще, по-хорошему, должно быть u(*args) или что-то типа того
 
     def x(self):
         """
@@ -50,7 +52,7 @@ class Muscle:
 
     def eq_F(self): 
         """
-        ODE for F variable
+        Right part of an ODE for F variable
         """
         F = self.F 
         tau1 = self.tau1 
@@ -60,29 +62,11 @@ class Muscle:
 
 
     # System of ODEs
-    def dX_dt(X, t): # Возможно, лучше заменить на model
-        'FIXME: Добавить нормальное описание '
+    def model(self, t): #
+        'Collects ODEs in one system'
 
-        #Constants
-        tauc = 0.1 #c
-        tau1 = 0.1 
-        k = 5
-        A = 10 
-        m = 2
+        eq_1 = self.eq_CN(t)
+        eq_2 = self.eq_F()
 
-        CN = X[0]
-        F = X[1] 
-
-        # по u ещё ни черта не понятно
-        def u(t):
-            return abs(np.sin(t * 2))
-
-        eq_1 = - CN/tauc + u(t)
-
-        def x(t):
-            return CN**m/(CN**m + k**m)
-        
-        eq_2 = - F/tau1 + A*x(t)
-
-        return np.array([eq_1, eq_2])
+        return array([eq_1, eq_2])
     
