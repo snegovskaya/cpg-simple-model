bb369e9a33fef83ae8e695e6a1c3b9772b358df2
Daria Snegovskaya <snegovskaya.da@gmail.com>
2024-02-28 09:33:37 +0300

    Added & commented the example

--- scripts/main_test.py
+++ scripts/main_test.py
@@ -1,56 +1,17 @@
-from src import muscle 
-from src import neuron
-
-# neuron.neuron_test()
-# muscle.muscle_test()
-# print("I'm main")
-
+from src.muscle import *
+from src.neuron import *
 
 from numpy import *
 from matplotlib import pyplot as p
 from scipy import integrate
 
-# t = linspace(0, 100, 1000)
-# C_N0 = 1 # Чему, ёлы-палы, может быть равен C_N?
-# F0 = 0
-# X0 = array([C_N0, F0])
-# vrest = -63.0540942 # По книге vrest = -65.0
-# mrest = 0.06 # По книге mrest = 0.05
-# nrest = 0.00 # По книге nrest = 0.00
-# hrest = 0.54 # По книге hrest = 0.62
-# Y0 = array([vrest,mrest,nrest,hrest])
-# t = linspace(0, 100, 1000) # Временная шкала
-# Iapp = 4
-
-# X =  integrate.odeint(muscle.dX_dt, X0, t) 
-# C_N, F = X.T
-# Y =  integrate.odeint(neuron.dX_dt, Y0, t, args=(Iapp,)) # args=(Iapp, tauInput)
-# v, m, n, h = Y.T 
-# def u(t):
-#     return  abs(sin(t*2))
-
-# fig1 = p.figure()
-# p.plot(t, v)
-# p.show(block=True)
-
-# fig2 = p.figure()
-# p.plot(t, u(t), label = '$u, \: \mathrm{мВ}$')
-# p.plot(t, F*500, label = '$F (x500), \: \mathrm{?}$')
-# p.legend()
-# p.xlabel('$t, \: \mathrm{мс}$')
-# p.ylabel('$u, \: F$')
-# p.ylim([-1, 1])
-# p.title('Мышечная сила и входное напряжение, зависимость от времени')
-# p.show(block=True)
-
 t = linspace(0, 100, 100)
 CN0 = 1
 F0 = 0
-def u(t):
-    return sin(t)
 
-print(size(u(t)))
+u = 1
 
-init_conds = array([CN0, F0])
-test_item = muscle.Muscle(CN0, F0, u)
-test_result = integrate.odeint(test_item.model, init_conds, t)
+test_muscle = Muscle(CN0, F0, u)
+# Таким образом имеем, что test_muscle.CN = CN0, test_muscle.F = F0 
+test_result = integrate.odeint(test_muscle.model, test_muscle.CN, test_muscle.F, t)
+# Дальше просто причесать результат и в продакшн
--- src/muscle.py
+++ src/muscle.py
@@ -1,8 +1,121 @@
-from numpy import *
+from numpy import * 
 
 def muscle_test():
     print("I'm a Simplified Adapted Model muscle")
     return
+
+# ===== Nessessary technical feature ======
+import weakref 
+
+class WeakMethod: # Та самая приблуда для обхода циклических ссылок
+    """
+    Объектами класса (лучше бы назвать этот класс WeakAttribute, конечно)
+    должны стать те стрёмные атрибуты CN и F, которые я 
+    из своего изначального класса Muscle собираюсь вытащить
+    """
+    def __init__(self, attribute, instance):
+        self.attribute = attribute 
+        self.instance_ref = weakref.ref(instance) 
+
+        # self.__wrapped__ # Есть в образце, но у меня  пока не прописан 
+
+    def __call__(self):
+        instance = self.instance_ref
+        return self.attribute(instance)
+
+    # def __repr__(self):
+    #     # Тоже зачем-то нужен, но у меня тоже пока не прописан 
+# =======================================   
+    
+# И теперь должно быть что-то типа того: 
+class Muscle: 
+    def __init__(self, CN0):
+        self.CN = # FIXME
+    
+    def eq_CN(self, )
+        
+
+# То есть хотим так:
+muscle_test = Muscle(CN)
+
+print(muscle_test.eq_CN(muscle_test.CN))
+# Короче, пока см. ниже
+
+
+# =============================
+# Пример со stackoverflow: 
+def foo1(self, b):
+    return self.a*b
+
+class FooBar1(object):
+    def __init__(self, func, a):
+        self.a=a
+        self.func=func
+
+# Now, if you try the following:
+foobar1 = FooBar1(foo1,4)
+foobar1.func(3) # = foo1(self, 3)
+# You'll get the following error:
+# TypeError: foo0() missing 1 required positional argument: 'b' 
+# т.е. типа 3 - это self
+
+import weakref
+
+class WeakMethod:
+    def __init__(self, func, instance): 
+        """ т.е. объект этого класса — это хреновина, которая связывает 
+        слабую ссылку на объект исходного класса с функцией, которую использует класс. 
+        Ещё раз: объект класса по слабой ссылке и функция для объекта этого класса
+        """
+        self.func = func
+        self.instance_ref = weakref.ref(instance)
+
+        self.__wrapped__ = func  # this makes things like `inspect.signature` work
+        # Я ХЗ, что это
+
+    def __call__(self, *args, **kwargs):
+        instance = self.instance_ref()
+        return self.func(instance, *args, **kwargs)
+    """ 
+    Т.е. объект этого класса — это автоматический вызов нужной нам функции 
+    от объекта исходного класса? 
+    """ 
+
+    """ 
+    В чём тогда твоя проблема, Карл? 
+    В том, что вызвать функцию исходного класса от самого класса ты не можешь, 
+    а вызвать эту же функцию от слабой ссылки на класс ты можешь? 
+    """
+
+    def __repr__(self):
+        cls_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(cls_name, self.func, self.instance_ref()) 
+    # На это тоже пока забьём
+
+
+class FooBar(object):
+    def __init__(self, func, a):
+        self.a = a
+        self.func = WeakMethod(func, self) 
+        """ 
+        То есть в переводе на русский, когда мы в этом (исходном) классе 
+        вызываем (дёргаем) функцию как атрибут, 
+        мы на самом деле вызываем её с аргументом – слабой ссылкой на сам класс 
+        (элементом класса). 
+        И эта чёртова магия должна сработать просто из-за волшебных слов «слабая ссылка»? 
+        Что за хрень?!
+        """ 
+
+f = FooBar(foo1, 7) 
+print(f.func(3))  # 21 
+""" = WeakMethod(foo1, f)(3) = foo1(weakref.ref(f), 3)) = 
+= weakref.ref(f).a * 3 = 7 * 3 = 21 
+Ну вот да, основной вопрос — зачем нужна вся эта хренотень со слабыми ссылками?
+"""
+
+# =====================================
+
+
 
 class Muscle: 
     # Simplified Adapted Model (see Wilson2013)
@@ -28,6 +141,7 @@ class Muscle:
         self.F = F0 
         self.u = u # 1. FIXME Но это не точно; 2. Зависимость от t сюда вроде как писать не нужно 
 
+
     def eq_CN(self, t): 
         """
         Right part of an ODE for CN variable
@@ -40,6 +154,7 @@ class Muscle:
         return - CN / tauc + u(t) 
         # FIXME: вообще, по-хорошему, должно быть u(*args) или что-то типа того
 
+
     def x(self):
         """
         Function x(CN)
@@ -49,6 +164,7 @@ class Muscle:
         k  = self.k 
 
         return CN ** m / (CN ** m + k ** m)
+
 
     def eq_F(self): 
         """
@@ -61,12 +177,14 @@ class Muscle:
         return - F / tau1 + A * x
 
 
-    # System of ODEs
-    def model(self, t): #
-        'Collects ODEs in one system'
+    def model(self, CN, F, t): 
+        """
+        Collects ODEs in one system
+        """
+        # Эту функцию пока фиксируем и от неё отталкиваемся
 
-        eq_1 = self.eq_CN(t)
-        eq_2 = self.eq_F()
+        eq_1 = self.eq_CN(CN, t)
+        eq_2 = self.eq_F(F, t)
 
         return array([eq_1, eq_2])
     
