02b3d5dce771863e873413bcce03b8417f8afd4e
Daria Snegovskaya <snegovskaya.da@gmail.com>
2024-06-04 01:06:42 +0300

    Added some ideas to assembly.py

--- scripts/main_test.py
+++ scripts/main_test.py
@@ -56,22 +56,22 @@ test_muscle = Muscle(CN0, F0, input = test_neuron.v - v0) # t=t[0]
   
 
 # Полностью собранная схема
-# def delegate_circuit(neuron, muscle, vars, t): 
-#     vars_neuron = vars[0:4]
-#     vars_muscle = vars[4:]
-#     model_neuron = delegate_Neuron(neuron, vars_neuron, t)
-#     model_muscle = delegate_Muscle(muscle, vars_muscle, t, input = neuron.v - v0) 
-#     return concatenate([model_neuron, model_muscle]) 
+def delegate_circuit(neuron, muscle, vars, t): 
+    vars_neuron = vars[0:4]
+    vars_muscle = vars[4:]
+    model_neuron = delegate_Neuron(neuron, vars_neuron, t)
+    model_muscle = delegate_Muscle(muscle, vars_muscle, t, input = neuron.v - v0) 
+    return concatenate([model_neuron, model_muscle]) 
 
-# vars0 = vars0_neuron + vars0_muscle
+vars0 = vars0_neuron + vars0_muscle
 
-# test_result = integrate.odeint(lambda *args: delegate_circuit(test_neuron, test_muscle, *args,), vars0, t)
-# v,m,n,h, CN, FN = test_result.T 
+test_result = integrate.odeint(lambda *args: delegate_circuit(test_neuron, test_muscle, *args,), vars0, t)
+v,m,n,h, CN, FN = test_result.T 
 
 # Строю кривую нейрона отдельно
 
-# neuron_vars = integrate.odeint(lambda *args: delegate_Neuron(test_neuron, *args,), vars0_neuron, t) 
-# v,m,n,h = neuron_vars.T 
+neuron_vars = integrate.odeint(lambda *args: delegate_Neuron(test_neuron, *args,), vars0_neuron, t) 
+v,m,n,h = neuron_vars.T 
 
 # # График v(t) для нейрона
 # fig = p.figure()
@@ -83,17 +83,17 @@ test_muscle = Muscle(CN0, F0, input = test_neuron.v - v0) # t=t[0]
 # p.show(block = True)
 
 
-# тест интегрирования одного уравнения CN при импульсном токе 
-def delegate_CN(muscle, CN, uinput, t):
-    muscle.CN = CN 
-    print('CN= ',CN) # Убрать потом
-    if muscle.upars.get('t') != None:
-        muscle.upars['t'] = t
-        muscle.u = uinput(**muscle.upars) 
-    else:
-        muscle.u = uinput
-        print('muscle.u = ', muscle.u) # Убрать потом
-    return muscle.eq_CN() 
+# # тест интегрирования одного уравнения CN при импульсном токе 
+# def delegate_CN(muscle, CN, uinput, t):
+#     muscle.CN = CN 
+#     print('CN= ',CN) # Убрать потом
+#     if muscle.upars.get('t') != None:
+#         muscle.upars['t'] = t
+#         muscle.u = uinput(**muscle.upars) 
+#     else:
+#         muscle.u = uinput
+#         print('muscle.u = ', muscle.u) # Убрать потом
+#     return muscle.eq_CN() 
 
 
 # def getvt(tarray=t, varray=v, **kwargs): # Вот этот набросок надо доработать 
@@ -109,43 +109,43 @@ def delegate_CN(muscle, CN, uinput, t):
 # CN = test_result.T[0]  
 
 
-# Попытка в сборку чисто с CN
+# # Попытка в сборку чисто с CN
 
-def delegate_circuit(neuron, muscle, vars, t):
-    print('t = ', t) # Убрать потом 
-    vars_neuron = vars[0:4]
-    CN = vars[4:]
-    model_neuron = delegate_Neuron(neuron, vars_neuron, t)
-    model_CN = delegate_CN(muscle, CN, neuron.v - v0, t) 
-    return concatenate([model_neuron, model_CN]) 
+# def delegate_circuit(neuron, muscle, vars, t):
+#     print('t = ', t) # Убрать потом 
+#     vars_neuron = vars[0:4]
+#     CN = vars[4:]
+#     model_neuron = delegate_Neuron(neuron, vars_neuron, t)
+#     model_CN = delegate_CN(muscle, CN, neuron.v - v0, t) 
+#     return concatenate([model_neuron, model_CN]) 
 
-vars0 = vars0_neuron + (CN0,)
-
-
-
-test_result = integrate.odeint(lambda *args: delegate_circuit(test_neuron, test_muscle, *args,), vars0, t)
-v, m, n, h, CN = test_result.T
+# vars0 = vars0_neuron + (CN0,)
 
 
 
-# # Самый главный график
-# fig = p.figure()
-# p.plot(t, [Iimpulse(tmeaning)*10 for tmeaning in t], label = 'Iapp') # масштаб x10 
-# p.plot(t, v*1e1, label = 'v')
-# p.plot(t, FN,label = 'FN')
-# # p.plot(t, FN,label = 'FN') # FIXME Тестовая строчка, удалить
-# p.plot(t, CN, label = 'CN')
-# p.xlabel('$t, \: \mathrm{мс}$')
-# p.legend()
-# p.title('Поведение потенциала на нейроне $v$ и мускульной силы $FN$ (x1000) \n при импульсном внешнем токе $I_{app}(t)$ \n амплитудой %.2f $\mathrm{мкА/см^2}$ (показана $10\mathrm{x}$) и длительностями импульса от %.1f до %.1f $\mathrm{мс}$' % (5, 1, 5))
-# p.show(block = True) 
+# test_result = integrate.odeint(lambda *args: delegate_circuit(test_neuron, test_muscle, *args,), vars0, t)
+# v, m, n, h, CN = test_result.T
 
-# График CN
+
+
+# Самый главный график
 fig = p.figure()
-p.plot(t, [Iimpulse(tmeaning) for tmeaning in t], label = 'Iapp')
+p.plot(t, [Iimpulse(tmeaning)*10 for tmeaning in t], label = 'Iapp') # масштаб x10 
 p.plot(t, v, label = 'v')
+p.plot(t, FN*1e2,label = 'FN')
+# p.plot(t, FN,label = 'FN') # FIXME Тестовая строчка, удалить
 p.plot(t, CN, label = 'CN')
 p.xlabel('$t, \: \mathrm{мс}$')
 p.legend()
-p.title('CN при импульсном внешнем токе $I_{app}(t)$ \n амплитудой %.2f $\mathrm{мкА/см^2}$ (показана $10\mathrm{x}$) и длительностями импульса от %.1f до %.1f $\mathrm{мс}$' % (5, 1, 5))
-p.show(block = True)
+p.title('Поведение потенциала на нейроне $v$ и мускульной силы $FN$ (x1000) \n при импульсном внешнем токе $I_{app}(t)$ \n амплитудой %.2f $\mathrm{мкА/см^2}$ (показана $10\mathrm{x}$) и длительностями импульса от %.1f до %.1f $\mathrm{мс}$' % (5, 1, 5))
+p.show(block = True) 
+
+# # График CN
+# fig = p.figure()
+# p.plot(t, [Iimpulse(tmeaning) for tmeaning in t], label = 'Iapp')
+# p.plot(t, v, label = 'v')
+# p.plot(t, CN, label = 'CN')
+# p.xlabel('$t, \: \mathrm{мс}$')
+# p.legend()
+# p.title('CN при импульсном внешнем токе $I_{app}(t)$ \n амплитудой %.2f $\mathrm{мкА/см^2}$ (показана $10\mathrm{x}$) и длительностями импульса от %.1f до %.1f $\mathrm{мс}$' % (5, 1, 5))
+# p.show(block = True)
