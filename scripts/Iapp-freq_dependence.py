e9559e6f0b32cf140cf8725044a965be9ed7a55b
Daria Snegovskaya <snegovskaya.da@gmail.com>
2025-07-03 03:16:06 +0300

    Receptor proceeding, first details

--- scripts/Iapp-freq_dependence.py
+++ scripts/Iapp-freq_dependence.py
@@ -1,4 +1,9 @@
-def get_v_freq(v): 
+from src.neuron import Neuron 
+from src.net import Net 
+from src.ode_system import ODE_system
+
+
+def get_v_freq(v): # FIXME: скопипастить Фурью из nemusrec'a 
     '''
     Фурье-обработка массива значений v;
     Дополнительно требуется время симуляции Т 
@@ -7,7 +12,7 @@ def get_v_freq(v):
     '''
     pass 
 
-def get_v(neuron, Iapp, t): 
+def get_v(neuron, Iapp, t): # FIXME: банально пофиксить
     '''
     Должен для переданного эл-та класса Neuron, 
     входного тока Iapp 
@@ -16,7 +21,12 @@ def get_v(neuron, Iapp, t):
     Требует импортирования класса Neuron 
     и модуля, который занимается интегрированием.
     '''
-    pass 
+    neuron = Neuron(input = I_const(*args, **kwargs)) 
+    neuron.net.set_matrix() 
+    ode_system = ODE_system()
+    result = ode_system.solution(t)
+    v, m, n, h, CN, F = result.T 
+    return v
 
 
 def freq_from_Iapp(): 
@@ -25,12 +35,19 @@ def freq_from_Iapp():
     На выходе хотим видеть для каждого Iapp доминирующую частоту; 
     Хотя полезнее сразу делать фиттинг функции и выдавать коэф-ты
     '''
-    pass 
+    for Iapp in Iapp_probes: #FIXME: банально пофиксить 
+        i = Iapp_probes.index(Iapp)
+        v_meanings[i] = get_v() 
+        v_maxes[i] = max(v_meanings[i]) 
+
+    plot = p.pyplot(Iapp_probes, v_maxes) 
+    return plot
+    # fit(plot, <certain_dependence>)
 
 
-def Iapp_from_freq(): 
+def Iapp_from_freq(depenence): # FIXME: конкретизировать черновик
     '''
     Для практического использования на выходе хотим видеть формулу пересчёта
     из частоты в Iapp — скорее всего функцию.
     '''
-    pass 
+    return reverse(dependence)
