303a4cb441c369d1b83e2df8c01b3cd8d657c840
Daria Snegovskaya <snegovskaya.da@gmail.com>
2025-08-05 13:42:52 +0300

    Got the required dependence

--- scripts/Iapp-freq_dependence.py
+++ scripts/Iapp-freq_dependence.py
@@ -7,7 +7,8 @@ from src.Iapp_patterns import I_const
 
 import numpy as np 
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq 
+from scipy.optimize import curve_fit
 
 
 def get_v_freq(v, T): # FIXME: скопипастить Фурью из nemusrec'a 
@@ -81,15 +82,21 @@ def freq_from_Iapp(T, N, Iapp_probes = np.linspace(0, 5, 50)):
         t = np.linspace(0, T, N)
         dt = np.diff(t)[0] # ms, шаг симуляции 
         v = get_v(Iapp, T, N) 
-        # plt.plot(t, v)
+
+        # plt.plot(t, v) # Отладочный график для малого кол-ва Iapp
+        # plt.title("Картина спайков при Iapp = %.2f мкА" %(Iapp))
         # plt.show(block = True)
+
         v_freq = get_v_freq(v, T) # FIXME А есть ли именно соответствующий метод для массива?
         v_freq = np.abs(v_freq[1:N//2]) # Предобработка под задачу поиска макс. частоты
         freq = fftfreq(N, dt)
         freq = freq[1:N//2] # Предобработка под задачу поиска макс. частоты
         # plt.plot(freq[1:N//2], np.abs(v_freq[1:N//2])) # Старая версия, без предобработки 
-        # plt.plot(freq, v_freq)
-        # plt.show(block = True)
+
+        # plt.plot(freq, v_freq) # Отладочный график для малого кол-ва Iapp
+        # plt.title("Фурье-разложение картины спайков при Iapp = %.2f мкА" %(Iapp))
+        # plt.show(block = True) 
+
         # dominant_frequencies.append(freq[np.argmax(np.abs(v_freq))]) # Старая версия, без предобработки 
         dominant_frequencies.append(freq[np.argmax(v_freq)])
          
@@ -97,7 +104,7 @@ def freq_from_Iapp(T, N, Iapp_probes = np.linspace(0, 5, 50)):
     plot = plt.plot(Iapp_probes, dominant_frequencies)
     plt.title("Зависимость частоты спайкинга $(мс^-1)$ на нейроне ХХ \
         \n от постоянного внешнего тока $I_{app}$ \
-        \n в диапазоне от %.0f до %.0f мкА на 50 точках" %(Iapp_probes[0], Iapp_probes[-1])) 
+        \n в диапазоне от %.2f до %.2f мкА на %i знач-ях $I_{app}$" %(Iapp_probes[0], Iapp_probes[-1], len(Iapp_probes))) 
     plt.xlabel('$Iapp, \: \mathrm{мкА}$') 
     plt.ylabel('$частота, \: \mathrm{мс}^{-1}$') 
     plt.show(block = True)
@@ -106,16 +113,19 @@ def freq_from_Iapp(T, N, Iapp_probes = np.linspace(0, 5, 50)):
     # fit(plot, <certain_dependence>)
 
 
-def Iapp_from_freq(depenence): # FIXME: конкретизировать черновик
+def Iapp_from_freq(Iapp_probes, dominant_frequencies): # FIXME: конкретизировать черновик
     '''
     Для практического использования на выходе хотим видеть формулу пересчёта
     из частоты в Iapp — скорее всего функцию.
-    '''
-    return reverse(dependence) 
+    ''' 
+    plt.plot(dominant_frequencies, Iapp_probes)
+    plt.title("Зависимость входного тока на нейроне \
+              \n от выходной частоты спайкинга данного нейрона")
+    return # reverse(dependence) 
 
 if __name__ == "__main__": 
     T = 500 # мс
-    N = 100 # точек 
+    N = 1000 # точек 
     Iapp1 = 1.15 
     Iapp2 = 1.25 
     # t = np.linspace(0, T, N) # FIXME: Надо шото сделать с дублированием здесь и в функции
@@ -130,9 +140,36 @@ if __name__ == "__main__":
     # v_freq = get_v_freq(v, T)
     # v1_freq = get_v_freq(v1, T)
     # v2_freq = get_v_freq(v2, T) # Вот это вот всё делалось для двух значений 
-    Iapp_min = 0 #мкА # Было 1 мкА
-    Iapp_max = 10 #мкА # Было 1.3 мкА
-    Iapp_n_points = 20 
+    Iapp_min = 0.38 #мкА # Было 1 мкА
+    Iapp_max = 2.88 #мкА # Было 1.3 мкА
+    Iapp_n_points = 10 
     Iapp_probes = np.linspace(Iapp_min, Iapp_max, Iapp_n_points)
-    freq_from_Iapp(T, N, Iapp_probes) 
-    # Iapp_from_freq()
+    dominant_frequencies = freq_from_Iapp(T, N, Iapp_probes) 
+    Iapp_from_freq(Iapp_probes, dominant_frequencies) 
+
+    def fitting_func(x, a0, a1, a2, a3, a4): 
+        return a0 * x**4 + a1 * x**3 + a2 * x**2 + a3 * x + a4
+    
+    pars, pars_cov = curve_fit(fitting_func, Iapp_probes, dominant_frequencies) 
+    fitting_freqs = list(map(lambda x: fitting_func(x, *pars), Iapp_probes)) # FIXME: Почему не работает array от map'а?
+    plt.plot(Iapp_probes, dominant_frequencies, label = 'фактическая зависимость') 
+    plt.plot(Iapp_probes, fitting_freqs, label = 'фиттинг')
+    plt.xlabel('$частота, \: \mathrm{мс}^{-1}$')
+    plt.ylabel('$Iapp, \: \mathrm{мкА}$') 
+    plt.title("Фиттирование зависимости входного тока на нейроне \
+              \n от его выходной частоты спайкинга") 
+    plt.legend()
+    plt.show(block = True) 
+    print("Значения пар-ров кривой фиттинга, \
+        \n многочлена 4 порядка: \
+        \n a0 = %.2E; \n a1 = %.2E; \n a2 = %.2E; \n a3 = %.2E; \n a4 = %.2E" \
+        %(pars[0], pars[1], pars[2], pars[3], pars[4])) 
+
+'''
+a0 = -9.24E-03; 
+ a1 = 6.13E-02; 
+ a2 = -1.32E-01; 
+ a3 = 1.26E-01; 
+ a4 = -2.44E-02
+''' 
+
